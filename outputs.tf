5bf1d756a8b403a4cf565f7d0b1ab3272106d554
Steve Cotugno <steve.cotugno@us.ibm.com>
2020-03-13 11:48:11 -0700

    comment out connection output for now

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "db_crn_id" {
   value = "${ibm_database.db_instance.id}"
 }
+/*  ** TO DO: - Check with Praveen on the output string
 output "db_connection_string" {
-  value = "http://${ibm_database.db_instance.connectionstrings[0].name}"
+  value = "http://${ibm_database.db_instance.connectionstring.[0].name}"
 }
+*/
 output "db_admin_userid" {
   value = "${ibm_database.db_instance.adminuser}"
 }
